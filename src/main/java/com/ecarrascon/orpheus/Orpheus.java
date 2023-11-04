201491afbfcdcd16f19c044e4503c993b4cc8700
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2023-11-04 20:47:29 +0100

    feature: add cow_gut drop to cow

--- src/main/java/com/ecarrascon/orpheus/Orpheus.java
+++ src/main/java/com/ecarrascon/orpheus/Orpheus.java
@@ -4,18 +4,13 @@ import com.ecarrascon.orpheus.entity.OrpheusEntities;
 import com.ecarrascon.orpheus.entity.custom.ViperEntity;
 import com.ecarrascon.orpheus.registry.BlocksRegistry;
 import com.ecarrascon.orpheus.registry.ItemsRegistry;
+import com.ecarrascon.orpheus.registry.LootRegistry;
 import com.ecarrascon.orpheus.villager.Villager;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
-import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,7 +24,6 @@ public class Orpheus implements ModInitializer {
 			() -> new ItemStack(ItemsRegistry.ORPHEUS_LYRE.get()));
 
 
-	private static final Identifier TURTLE_LOOT_TABLE_ID = EntityType.TURTLE.getLootTableId();
 
 	@Override
 	public void onInitialize() {
@@ -38,7 +32,7 @@ public class Orpheus implements ModInitializer {
 		Villager.registerVillager();
 		Villager.registerVillagerTradeOffer();
 		Villager.registerPhilosopherHouses();
-		lootTablesIni();
+		LootRegistry.registerAll();
 
 
 		FabricDefaultAttributeRegistry.register(OrpheusEntities.VIPER, ViperEntity.createViperAttributes());
@@ -46,17 +40,4 @@ public class Orpheus implements ModInitializer {
 	}
 
 
-	protected void lootTablesIni() {
-
-		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-			if (source.isBuiltin() && TURTLE_LOOT_TABLE_ID.equals(id)) {
-				LootPool.Builder poolBuilder = LootPool.builder()
-						.conditionally(RandomChanceLootCondition.builder(0.3f))
-						.with(ItemEntry.builder(Items.SCUTE));
-
-				tableBuilder.pool(poolBuilder);
-			}
-		});
-
-	}
 }
