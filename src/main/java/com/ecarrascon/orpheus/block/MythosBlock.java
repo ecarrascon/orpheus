5fe50d6ee3d4957743259908162e94a8f7e69485
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2023-11-12 19:38:05 +0100

    feature: add mythos way to get Orpheus Lyre

--- src/main/java/com/ecarrascon/orpheus/block/MythosBlock.java
+++ src/main/java/com/ecarrascon/orpheus/block/MythosBlock.java
@@ -3,6 +3,7 @@ package com.ecarrascon.orpheus.block;
 import com.ecarrascon.orpheus.registry.ItemsRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
@@ -20,23 +21,34 @@ public class MythosBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if (!world.isClient() && entity instanceof PlayerEntity player) {
-            if ( player.getMainHandStack().isItemEqual(ItemsRegistry.HELLENIC_CODEX.get().getDefaultStack())
+            // Get Calliope's Love
+            if (player.getMainHandStack().isItemEqual(ItemsRegistry.HELLENIC_CODEX.get().getDefaultStack())
                     && player.isSneaking() && isSurroundedByFlowers(world, pos)) {
                 summonLightning(player, world);
                 player.getMainHandStack().decrement(1);
                 giveCalliopesLoveItem(player, world);
-                world.breakBlock(pos,false);
-        }
-    }
-        super.onSteppedOn(world, pos, state, entity);
+                world.breakBlock(pos, false);
+            }
 
-}
+            // Get Orpheus Lyre
+            if (world.getRegistryKey().equals(World.NETHER) && player.getMainHandStack().isItemEqual(ItemsRegistry.APOLLOS_SON.get().getDefaultStack())
+                    && player.isSneaking() && isSurroundedByMagma(world, pos)) {
+                summonLightning(player, world);
+                player.getMainHandStack().decrement(1);
+                giveOrpheusLyreItem(player, world);
+                world.breakBlock(pos, false);
+            }
+        }
+        super.onSteppedOn(world, pos, state, entity);
+    }
+
     private void summonLightning(PlayerEntity player, World world) {
         LightningEntity lightning = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
         lightning.setCosmetic(true);
         lightning.setPosition(player.getPos());
         world.spawnEntity(lightning);
     }
+
     private boolean isSurroundedByFlowers(World world, BlockPos pos) {
         int yPos = pos.getY() + 1;
 
@@ -57,8 +69,35 @@ public class MythosBlock extends Block {
         return true;
     }
 
+    private boolean isSurroundedByMagma(World world, BlockPos pos) {
+        int yPos = pos.getY();
+
+        int startX = pos.getX() - 1;
+        int startZ = pos.getZ() - 1;
+        int endX = pos.getX() + 1;
+        int endZ = pos.getZ() + 1;
+
+        for (int x = startX; x <= endX; x++) {
+            for (int z = startZ; z <= endZ; z++) {
+                if (pos.getX() == x && pos.getZ() == z) continue;
+                BlockState blockState = world.getBlockState(new BlockPos(x, yPos, z));
+                if (!blockState.equals(Blocks.MAGMA_BLOCK.getDefaultState())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     private void giveCalliopesLoveItem(PlayerEntity player, World world) {
         ItemStack calliopesLove = ItemsRegistry.CALLIOPES_LOVE.get().getDefaultStack();
+        if (!player.getInventory().insertStack(calliopesLove)) {
+            Block.dropStack(world, player.getBlockPos(), calliopesLove);
+        }
+    }
+
+    private void giveOrpheusLyreItem(PlayerEntity player, World world) {
+        ItemStack calliopesLove = ItemsRegistry.ORPHEUS_LYRE.get().getDefaultStack();
         if (!player.getInventory().insertStack(calliopesLove)) {
             Block.dropStack(world, player.getBlockPos(), calliopesLove);
         }
