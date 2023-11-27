1b289ab02cc8a225db1e0a34ce745e107bbcf8cd
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2023-11-27 22:15:11 +0100

    feature: add orpheus lyre power, be immune to arrows

---
+++ src/main/java/com/ecarrascon/orpheus/event/ArrowImmuneEvent.java
@@ -0,0 +1,19 @@
+package com.ecarrascon.orpheus.event;
+
+import com.ecarrascon.orpheus.Orpheus;
+import com.ecarrascon.orpheus.registry.ItemsRegistry;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class ArrowImmuneEvent implements ServerLivingEntityEvents.AllowDamage{
+    @Override
+    public boolean allowDamage(LivingEntity entity, DamageSource source, float amount) {
+        return !(entity instanceof PlayerEntity player)
+                || !source.isOf(DamageTypes.ARROW)
+                || !player.getInventory().contains(ItemsRegistry.ORPHEUS_LYRE.get().getDefaultStack())
+                || !Orpheus.CONFIG_VALUES.getOrpheusLyrePower().matches(".*(?:protect|both).*");
+    }
+}
